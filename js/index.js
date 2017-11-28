a49630a541feb6110af42c8e6aa0b1c7d9c4e66c
jesus <jesusalas18@gmail.com>
2017-11-28 00:03:22 -0700

    Se definio primer parte de la compra de boletos

--- js/index.js
+++ js/index.js
@@ -5,6 +5,15 @@ $(document).ready(function(){
 
 $(".dropdown-button").dropdown();
 
+ $('.datepicker').pickadate({
+    selectMonths: true, // Creates a dropdown to control month
+    selectYears: 15, // Creates a dropdown of 15 years to control year,
+    today: 'Today',
+    clear: 'Clear',
+    close: 'Ok',
+    closeOnSelect: false // Close upon selecting a date,
+ });
+
 
 var inicio = document.getElementById('inicio');
 var nosotros = document.getElementById('nosotros');
