f2b313c0b51b1fe1846ac3490e287d836a42a3d6
Jesús Salas <jesusalas18@gmail.com>
2017-11-28 17:57:47 -0700

    Se estructuro pagina de pago

--- js/index.js
+++ js/index.js
@@ -5,15 +5,19 @@ $(document).ready(function(){
 
 $(".dropdown-button").dropdown();
 
- $('.datepicker').pickadate({
-    selectMonths: true, // Creates a dropdown to control month
-    selectYears: 15, // Creates a dropdown of 15 years to control year,
-    today: 'Today',
-    clear: 'Clear',
-    close: 'Ok',
-    closeOnSelect: false // Close upon selecting a date,
- });
+$('.datepicker').pickadate({
+  selectMonths: true, // Creates a dropdown to control month
+  selectYears: 15, // Creates a dropdown of 15 years to control year,
+  today: 'Today',
+  clear: 'Clear',
+  close: 'Ok',
+  closeOnSelect: false // Close upon selecting a date,
+});
 
+$(document).ready(function() {
+	$('select').material_select();
+});
+        
 
 var inicio = document.getElementById('inicio');
 var nosotros = document.getElementById('nosotros');
